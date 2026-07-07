c84242c9a862dc213c59b71cf14cf3d67124f8bd
Marcos Fernandes <fernandesmarcos.programador@gmail.com>
2026-07-07 18:16:39 -0300

    feat: adiciona anotações da aula sobre Equals

---
+++ src/aulas/colecoes/dominio/Smartphone.java
@@ -0,0 +1,42 @@
+package aulas.colecoes.dominio;
+
+public class Smartphone {
+    private String serialNumber;
+    private String marca;
+
+    public Smartphone(String serialNumber, String marca) {
+        this.serialNumber = serialNumber;
+        this.marca = marca;
+    }
+
+    // Sobrescrita
+    // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
+    // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
+    // Transitividade: para x,y,z diferente de null, se x.equals(y) == true, e x.equals(z) == true logo, y.equals(z) == true
+    // Consistente: x.equals(x) sempre retorna true se x for diferente de null
+    // para x diferente de null, x.equals(null) tem que retornar false.
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return  false;
+        if (this == obj) return true;
+        if (this.getClass() != obj.getClass()) return false;
+        Smartphone smartphone = (Smartphone) obj;
+        return serialNumber != null && serialNumber.equals(smartphone.serialNumber);
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+}
---
+++ src/aulas/colecoes/equals/EqualsTest01.java
@@ -0,0 +1,26 @@
+package aulas.colecoes.equals;
+
+import aulas.colecoes.dominio.Smartphone;
+
+public class EqualsTest01 {
+    public static void main() {
+        String nome = "Marcos Fernandes";
+        String nome2 = "Marcos Fernandes";
+        String nome3 = new String("Marcos Fernandes");
+        System.out.println(nome == nome2); /* retorna: true */
+        System.out.println(nome == nome3); /* retorna: false */
+        System.out.println(nome.equals(nome3)); /* retorna: true */
+        System.out.println("--------------------------");
+        System.out.println(" ");
+
+        System.out.println("Smartphone:");
+        System.out.println("----------------------------");
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("1ABC1", "iPhone");
+//      System.out.println(s1.equals(s2));  assim da false pois cada variável faz referência a objetos diferentes OBS: sem a sobrescrita do equals
+        System.out.println(s1.equals(s2));
+
+
+
+    }
+}
